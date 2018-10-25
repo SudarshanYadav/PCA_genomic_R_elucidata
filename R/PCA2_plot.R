8b2432856435898a88ee3cf50381e8ad3662f5a2
sudarshanYadav <sudarshanyadav1997@gmail.com>
2018-10-25 07:13:00 +0530

    Add loadings plot

--- R/PCA2_plot.R
+++ R/PCA2_plot.R
@@ -5,8 +5,13 @@
 #' @export
 #' @param file_1 main genomic file
 #' @param file_2 metadata file
-PCA2_plot <- function(file_1,file_2,...) {
-  library(ggfortify)
+PCA2_plot <- function(file_1,file_2,loadings=FALSE,...) {
+
+  list.of.packages <- c("plotly")
+  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+  if(length(new.packages)) install.packages(new.packages)
+
+  library(plotly)
 
   if(!grepl(".csv$", file_1)){
     stop("Uploaded genomic file must be a .csv file!")
@@ -19,22 +24,28 @@ PCA2_plot <- function(file_1,file_2,...) {
   metadata = read.csv(file_2)
 
   #make gene names as row names
-  rownames(genomic_data) = make.names(genomic_data[,2],unique = TRUE)
-  genomic_data = genomic_data[,3:ncol(genomic_data)]
   genomic_data = as.data.frame(sapply(genomic_data,as.numeric))
   genomic_data = na.omit(genomic_data)
+  rownames(genomic_data) = make.names(genomic_data[,2],unique = TRUE)
+  genomic_data = genomic_data[,3:ncol(genomic_data)]
+
   t_genomic_data = as.data.frame(t(genomic_data))
   t_genomic_data = na.omit(t_genomic_data)
 
-  t_genomic_data.pca = prcomp(t_genomic_data,scale. = TRUE,center = TRUE)
-
-  #fviz_pca_var(t_genomic_data.pca,
-  #             col.var = "contrib", # Color by contributions to the PC
-  #             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
-  #             repel = TRUE )
+  genomic_data.pca = prcomp(genomic_data,scale. = TRUE,center = TRUE)
 
 
-  autoplot(t_genomic_data.pca,data=metadata,colour="Time")
+  if(!loadings){
+    plot_ly(as.data.frame(genomic_data.pca$x), x = ~PC1, y = ~PC2,text = paste("Gene : ", rownames(genomic_data)))
+  }else{
+    plot_ly(as.data.frame(genomic_data.pca[2]), x = ~rotation.PC1, y = ~rotation.PC2,mode="lines+marker",color = as.character(metadata$Time),text = paste("Sample : ",metadata$sIdx,"\nTime : ",metadata$Time," ",metadata$Unit))
+  }
 
 }
+
+
+
+
+
+
 
